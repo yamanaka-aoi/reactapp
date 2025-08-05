feae57b4a7088307aa8289ffb4c79b76f724587a
aoi <ysa461@icloud.com>
2025-08-06 02:11:21 +0900

    ボタンと画面遷移練習

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+
 import './Start.css';
+import { useNavigate } from 'react-router-dom';
+
 
 const App = () => {
+  const navigate = useNavigate();
   return (
-    <div className="App">
-      <p>計算アプリ</p>
-    </div>
+    <>
+      <div className="title">
+        <p1>計算アプリ</p1>
+      </div>
+
+      <div className="button-group">
+        <button onClick={() => navigate('/app2')}>ボタン1</button>
+        <button>ボタン2</button>
+      </div>
+      
+    </>
   );
 }
 
---
+++ src/App2.js
@@ -0,0 +1,11 @@
+
+const App2 = () => {
+  return (
+    <div>
+      <h2>App2ページです</h2>
+      <p>ここはボタン1で遷移したページです。</p>
+    </div>
+  );
+}
+
+export default App2;
